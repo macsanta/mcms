f6a805b152a5cad7175cd52f5a42ba7c793b5956
macsanta <orest.macsanta@gmailcom>
2016-03-14 19:26:08 +0100

    MCMS v0.01

---
+++ config.inc.php
@@ -0,0 +1,10 @@
+<?php
+    define('ROOT_PATH', dirname(__FILE__));
+    define('DB_HOST', ''); //DataBase Host
+    define('DB_USER', ''); //DataBase User
+    define('DB_PASS', ''); //DataBase Password
+    define('DB_NAME', ''); //DataBase Name
+    define('BASE_URL', '/ira_api2'); //Example: /macsanta-cms
+    define('TITLE', 'BLOG');
+    define('TEMPLATE', 'default');
+?>
---
+++ modules/MainModule.php
@@ -0,0 +1,16 @@
+<?php
+/**
+*  Main module
+*  File: MainModule.php
+*  Author: Orest Oliinyk
+**/
+
+Class MainModule extends AppModule
+{
+
+  public function index()
+  {
+    display('Main/main.tpl');
+  }
+
+}
